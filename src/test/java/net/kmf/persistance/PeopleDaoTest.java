5591d16f11058a03fbe34da0473909d692d7e8d2
bswanson <brookeswanson09@gmail.com>
2017-11-04 10:55:05 -0500

    Creates people dao test.

--- src/test/java/net/kmf/persistance/PeopleDaoTest.java
+++ src/test/java/net/kmf/persistance/PeopleDaoTest.java
@@ -1,44 +1,54 @@
 package net.kmf.persistance;
 
+import net.kmf.entity.People;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class PeopleDaoTest {
+    PeopleDao peopleDao;
+    List<People> people;
+    int numberOfUsers;
 
     @Before
     public void setUp() throws Exception {
+        peopleDao = new PeopleDao();
+        people = peopleDao.getAllPeoples();
+        numberOfUsers = people.size();
+
 
     }
 
     @Test
-    public void getAllPeoples() throws Exception {
+    public void getAllPeoples() {
 
     }
 
     @Test
-    public void getPeople() throws Exception {
+    public void getPeople() {
 
     }
 
     @Test
-    public void getPeoplesByLastName() throws Exception {
+    public void getPeoplesByLastName() {
 
     }
 
     @Test
-    public void insert() throws Exception {
+    public void insert() {
 
     }
 
     @Test
-    public void update() throws Exception {
+    public void update() {
 
     }
 
     @Test
-    public void delete() throws Exception {
+    public void delete() {
 
     }
 

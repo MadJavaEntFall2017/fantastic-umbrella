09fca5f3080f1af8f935ba795e776aa5f5a521bc
bswanson <brookeswanson09@gmail.com>
2017-11-16 13:11:14 -0600

    Adds javadoc to PersonServlet.doGet

--- src/main/java/net/kmf/controller/PersonServlet.java
+++ src/main/java/net/kmf/controller/PersonServlet.java
@@ -1,9 +1,9 @@
 package net.kmf.controller;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.kmf.entity.Person;
-import net.kmf.persistance.PersonDao;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -21,25 +21,41 @@ import java.io.IOException;
 
 import java.util.List;
 
+/**
+ * The type Person servlet.
+ * The purpose of this servlet is to demonstrate the celebrities existing in the database.
+ *
+ * @author bswanson 11/15/2017
+ */
 @WebServlet (
         name = "persons",
         urlPatterns = {"/persons"}
 )
 public class PersonServlet extends HttpServlet {
+    /**
+     * This method responds to the get requests for the persons url.
+     * @param req The request object
+     * @param resp The response object
+     * @throws ServletException if something goes wrong in general
+     * @throws IOException if it can't parse the JSON
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PersonDao dao = new PersonDao();
-
+        // Empty list of to hold the people
         List<Person> people = null;
-        people = dao.getAllPeoples();
-//        Client client1 = ClientBuilder.newClient();
-//        WebTarget serviceWeb1 = client1.target("http://localhost:8080/kmf/api/person");
-//        Response response1 = serviceWeb1.request().get();
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        people = objectMapper.readValue(response1.toString(), List.class);
+
+        // Make the call to the API (Deployed on AWS)
+        Client client1 = ClientBuilder.newClient();
+        WebTarget serviceWeb1 = client1.target("http://18.221.209.59:8080/kmf/api/person");
+        Response response1 = serviceWeb1.request().get();
+
+        // Take the response and map it into people objects
+        ObjectMapper objectMapper = new ObjectMapper();
+        people = objectMapper.readValue(response1.readEntity(String.class), new TypeReference<List<Person>>() {});
+
+        // Send the people to the webpage to be displayed
         HttpSession session = req.getSession();
         session.setAttribute("people", people);
-        // send object to webpage
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/persons.jsp");
         dispatcher.forward(req, resp);
     }

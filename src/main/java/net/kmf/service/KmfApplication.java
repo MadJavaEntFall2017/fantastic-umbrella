4bd4693f1a0df2fc6d7f9635280958deea9a20fa
bswanson <brookeswanson09@gmail.com>
2017-11-16 13:23:33 -0600

    Adds javadoc to the KMFApplication class.

--- src/main/java/net/kmf/service/KmfApplication.java
+++ src/main/java/net/kmf/service/KmfApplication.java
@@ -4,10 +4,13 @@ package net.kmf.service;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * The basic application path kmf/api is the base level.
+ *
+ * @author jskarha 10/25/2017
+ */
 @ApplicationPath("/api")
 
 public class KmfApplication extends Application {
-
-
 
 }
